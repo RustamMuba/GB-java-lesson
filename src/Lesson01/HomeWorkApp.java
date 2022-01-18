c71c2e8da7c7cf4140f9d1ef64bdbc3b5b9fcdbc
RustamMuba <97903216+RustamMuba@users.noreply.github.com>
2022-01-18 12:30:48 +0500

    Add files via upload

---
+++ src/Lesson01/HomeWorkApp.java
@@ -0,0 +1,41 @@
+package Lesson01;
+
+public class HomeWorkApp {
+    public static void main(String[] args) {
+        //       System.out.println("Orange");
+        //       System.out.println("Banana");
+        //       System.out.println("Apple");
+
+//        int a;
+//        int b;
+//        int c = 0;
+//        a = 24;
+        //       b = 2;
+//       if (c == 24 - 2)
+//        {
+//            System.out.println("ab < 0! Отрицательное значение");
+//        }else
+        //        System.out.println("ab >= 0! Положительное значение");
+        //   }
+//}
+ //       class color {
+ //           public void color (){
+ //               int value = 10;
+ //               System.out.println("value <= 0! RED");
+ //               System.out.println("value [1;100)! Yellow");
+ //               System.out.println("value => 100! Green");
+ //           }
+ //       }
+//        int a;
+//        int b;
+//        a = 2;
+//        b = 3;
+//       if (a >=b)
+//        {
+ //           System.out.println("a >= b! ");
+ //       }else
+ //               System.out.println("a < b! ");
+ //         }
+}
+
+

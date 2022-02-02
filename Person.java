062ab3a0cdbe63f31017a66f579da0849ae67d2e
RustamMuba <97903216+RustamMuba@users.noreply.github.com>
2022-02-02 15:24:03 +0500

    Add files via upload

---
+++ Person.java
@@ -0,0 +1,46 @@
+package Lesson05.home;
+
+public class Person {
+private static final int CURRENT_YEAR = 2021;
+
+private String surname;
+    private String secondName;
+    private String name;
+    private String position;
+    private String phone;
+    private int salary;
+    private int birthYear;
+
+//2. Конструктор класса должен заполнять эти поля при создании объекта.
+public Person(String name,
+              String secondName,
+              String surname,
+              String phone,
+              String position,
+              int salary,
+              int birthYear) {
+    this.birthYear = birthYear;
+    this.name = name;
+    this.surname = surname;
+    this.secondName = secondName;
+    this.position = position;
+    this.phone = phone;
+    this.salary = salary;
+}
+//3.    Внутри класса «Сотрудник» написать метод, который выводит информацию об объекте в консоль.
+int getAge() {
+    return CURRENT_YEAR - birthYear;
+}
+int getSalary() {
+    return salary;
+}
+String getFullInfo() {
+    return  this.name + " " +
+        this.secondName + " " +
+        this.surname + " " +
+        this.getAge() + " years old, " +
+        this.position + ". Phone number: " +
+        this.phone + ". Salary is " +
+        this.getSalary() + " RU";
+}
+}

3867fccee53b527f2b58c215d7a74c556be77c21
RustamMuba <97903216+RustamMuba@users.noreply.github.com>
2022-02-28 16:50:08 +0500

    Add files via upload

---
+++ Homework003/src/Main.java
@@ -0,0 +1,57 @@
+import fruits.Apple;
+import fruits.Box;
+import fruits.Orange;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+        String[] arr = {"1", "2"};
+        changeArrayElements(arr, 0, 1);
+        System.out.println(Arrays.toString(arr));
+
+        Box<Apple> appleBox = new Box<>(makeNewApples(10));
+        Box<Orange> orangeBox = new Box<>(makeNewOranges(10));
+        Box<Orange> secondOrangeBox = new Box<>(makeNewOranges(0));
+
+        appleBox.compare(orangeBox);
+        appleBox.compare(secondOrangeBox);
+        orangeBox.transferTo(secondOrangeBox);
+        appleBox.compare(orangeBox);
+        appleBox.compare(secondOrangeBox);
+    }
+
+    public static ArrayList<Apple> makeNewApples(int count) {
+        ArrayList<Apple> fruits = new ArrayList<>();
+        for (int i = 0; i < count; i++) {
+            fruits.add(new Apple());
+        }
+        return fruits;
+    }
+
+    public static ArrayList<Orange> makeNewOranges(int count) {
+        ArrayList<Orange> fruits = new ArrayList<>();
+        for (int i = 0; i < count; i++) {
+            fruits.add(new Orange());
+        }
+        return fruits;
+    }
+
+
+    public static <T>void changeArrayElements(T[] array, int firstIndex, int secondIndex) {
+        if (firstIndex < 0 || array.length <= firstIndex || secondIndex < 0 || array.length <= secondIndex || array.length < 2) {
+            System.out.println("Некорректные индексы");
+            return;
+        }
+
+        // Если if-а не будет
+        try {
+            T arrayElement = array[firstIndex];
+            array[firstIndex] = array[secondIndex];
+            array[secondIndex] = arrayElement;
+        } catch (ArrayIndexOutOfBoundsException ex) {
+            System.out.println("Некорректные индексы");
+        }
+    }
+}

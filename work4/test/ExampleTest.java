cadc1f794beb7dcdc9ea7e25570022135ced6118
RustamMuba <97903216+RustamMuba@users.noreply.github.com>
2022-09-01 18:21:01 +0500

    Add files via upload

---
+++ work4/test/ExampleTest.java
@@ -0,0 +1,47 @@
+package work4.test;
+
+import org.junit.jupiter.api.Test;
+import work4.Response;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.CoreMatchers.containsString;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+public class ExampleTest extends AbstractTest {
+
+    @Test
+    void getRecipePositiveTest() {
+        given().spec(getRequestSpecification())
+            .when()
+            .get("https://api.spoonacular.com/recipes/716411/information")
+            .then()
+            .spec(responseSpecification);
+    }
+
+
+    @Test
+    void getAccountInfoWithExternalEndpointTest(){
+        Response response = given().spec(requestSpecification)
+            .when()
+            .formParam("title","Burger")
+            .post("https://api.spoonacular.com/recipes/cuisine").prettyPeek()
+            .then()
+            .extract()
+            .response()
+            .body()
+            .as(Response.class);
+        assertThat(response.getCuisine(), containsString("American"));
+    }
+
+    @Test
+    void test(){
+        given().spec(requestSpecification)
+            .when()
+            .formParam("title","Burger")
+            .formParam("language", "en")
+            .post("https://api.spoonacular.com/recipes/cuisine").prettyPeek()
+            .then()
+            .statusCode(200);
+    }
+
+}
